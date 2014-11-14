2ffa4d9b311e2bda7b9971eac6ea00d0853fa53c
Yuya ONUMA <yohnuma@gion-xy.net>
2014-11-15 01:20:03 +0900

    add linux-c settings

---
+++ conf/12-linux-c.el
@@ -0,0 +1,9 @@
+(defun linux-c-mode ()
+  "C mode with adjusted defaults for use with the Linux kernel."
+  (interactive)
+  (c-mode)
+  (c-set-style "K&R")
+  (setq tab-width 8)
+  (setq indent-tabs-mode t)
+  (setq c-basic-offset 8))
+(setq auto-mode-alist (cons (cons "\\.c$" 'linux-c-mode) auto-mode-alist))
