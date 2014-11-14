fc4e56fa7e4775b6e1ecde4643fc674a0e520b5b
Yuya ONUMA <yohnuma@gion-xy.net>
2014-11-15 01:19:39 +0900

    add auto-install settings

--- conf/20-package.el
+++ conf/20-package.el
@@ -7,3 +7,18 @@
 ;;; http://rubikitch.com/2014/09/09/use-package/
 ;;; (package-install use-package)
   (require 'use-package))
+
+;;;; auto-install
+(use-package auto-install
+  :config
+  (progn
+    ;; set install directory
+    (setq auto-install-directory "~/.emacs.d/site-lisp/")
+    ;; get elisp name for EmacsWiki
+    (auto-install-update-emacswiki-package-name t)
+    ;; enable function "install-elisp"
+    (auto-install-compatibility-setup)))
+
+;;;; exec-path-from-shell.el
+;;;; https://github.com/purcell/exec-path-from-shell
+(exec-path-from-shell-initialize)
